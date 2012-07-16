5c9c2a2cbf76ce82676a3ba0cfa88e3b5c40427d
Chris <chris@1337.af>
2012-07-16 01:07:53 +0000

    Added some documentation

--- jquery.expander.js
+++ jquery.expander.js
@@ -1,4 +1,10 @@
-/* 
+/* jQuery List expander expands lists of the form:
+ * 
+ * class item +
+ *            |- class headline +  (switcher and helper are attached here)
+ *            |                 |- class title (The title of the item)
+ *            |
+ *            |- class content (The actual content which should be expanded)
  * 
  * 
  * For usage and examples view:
@@ -14,22 +20,18 @@
   $.fn.expander = function(options) {
     // Default settings
     var settings = $.extend( {
-      'helper': true,		// If the Helpers should be attached
-      'slide': 300,
-      'opened': '-',
-      'closed': '+',
-      'helperClosed': '<- Click to unfold',
-      'helperOpened': '<- Click to fold'
+      'helper': true,				// If the Helpers should be attached
+      'slide': 300,				// Sliding speed
+      'opened': '-',				// the switcher text for an opened item
+      'closed': '+',				// the switcher text for a closed item
+      'helperClosed': '<- Click to unfold',	// the helper text for a closed item
+      'helperOpened': '<- Click to fold'	// the helper text for an opened item
     }, options);
     
     return this.each(function(){
-      // Hide the content
+      // Make initial changes that users without jQuery do not need to see
       $(this).find('.content').hide();
-      
-      // Attach the switcher
       $(this).find('.item .headline').append('<div class="switcher">' + settings['closed'] + '</div>');
-      
-      // Attach the Helper to the first item
       if(settings['helper']){
 	$(this).find('.headline:first').append('<div class="helper">' + settings['helperClosed'] + '</div>');
       }
@@ -37,6 +39,7 @@
       // At first all items are closed
       $(this).children('.item').addClass('closed');
       
+      // In case one of the switchers is clicked
       $('.switcher').click(function(){
 	$clicked = $(this).parent().parent();
 	
@@ -46,9 +49,6 @@
 	  $clicked.addClass('opened');
 	  $clicked.children('.content').slideDown(settings['slide']);
 	  $clicked.find('.switcher').html(settings['opened']);
-	  if(settings['helper']){
-	    helper($clicked.parent());
-	  }
 	}
 	// If it is opened, close it
 	else{
@@ -56,9 +56,10 @@
 	  $clicked.addClass('closed');
 	  $clicked.children('.content').slideUp(settings['slide']);
 	  $clicked.find('.switcher').html(settings['closed']);
-	  if(settings['helper']){
-	    helper($clicked.parent());
-	  }
+	}
+
+	if(settings['helper']){
+	  helper($clicked.parent());
 	}
       });
       
@@ -70,8 +71,8 @@
 	// Remove all helpers
 	$list.find('.helper').remove();
 	
-	$heads = $list.find('.headline');
 	// Attach them where needed
+	$heads = $list.find('.headline');
 	$heads.each(function(){
 	  if($firstClosed && $(this).parent().hasClass('closed')){
 	    $(this).append('<div class="helper">' + settings['helperClosed'] + '</div>');
